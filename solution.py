a222e080ad5a3d2f9a08d9f9820292818bf82ae1
Damai-SHAKTI <82355463+Damai-SHAKTI@users.noreply.github.com>
2021-05-10 21:23:31 +0800

    Create solution.py

---
+++ solution.py
@@ -0,0 +1,14 @@
+# Solution in python
+
+def addUpTo(n):
+    return n * (n + 1) / 2
+
+
+try:
+    n = int(input("Enter A Positive Number: "))
+    if n <= 0:
+        print("Not A Positive Integer!")
+    else:
+        print("Sum Of {N}: {sum}").format(N=n, sum=addUpTo(n))
+except ValueError:
+    print("Invalid Input!")
