9e076d312ed108bb0ff3aeb1bf78983c08e4b302
Damai-SHAKTI <82355463+Damai-SHAKTI@users.noreply.github.com>
2021-05-10 21:08:32 +0800

    Update solution.cs

--- solution.cs
+++ solution.cs
@@ -1,3 +1,4 @@
+// Solution in C#
 using System;
 
 namespace ConsoleApp
@@ -6,7 +7,24 @@ namespace ConsoleApp
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine(AddUpTo(3));
+            try
+            {
+                Console.Write("Input A Positive Number: ");
+                int n = Convert.ToInt32(Console.ReadLine());
+                if (n <= 0)
+                {
+                    Console.WriteLine("Not A Positive Integer!");
+                }
+                else
+                {
+                    Console.WriteLine("Sum of {0}: {1}", n, AddUpTo(n));
+                }
+            } 
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid Input!");
+            }
+
         }
 
         public static int AddUpTo(int n)
